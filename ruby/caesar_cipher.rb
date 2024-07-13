375ca9c401badee70a9048307a7fcf53597bb810
Tom Fishwick <fishwickt@gmail.com>
2024-07-13 20:02:25 +0100

    Started tictactoe

--- ruby/caesar_cipher.rb
+++ ruby/caesar_cipher.rb
@@ -1,17 +1,12 @@
-
 def self.encode_letter(letter_to_encode, shift_amount, letterToNumber, numberToLetter, alphabet_length)
-    is_upper = letter_to_encode.upcase == letter_to_encode
-    if letterToNumber.key?(letter_to_encode.downcase)
-        new_number = (letterToNumber[letter_to_encode.downcase] + shift_amount) % alphabet_length
-        new_letter = numberToLetter[new_number]
-        if is_upper
-            return new_letter.upcase
-        else
-            return new_letter
-        end
-    else
-        return letter_to_encode
-    end
+  is_upper = letter_to_encode.upcase == letter_to_encode
+  return letter_to_encode unless letterToNumber.key?(letter_to_encode.downcase)
+
+  new_number = (letterToNumber[letter_to_encode.downcase] + shift_amount) % alphabet_length
+  new_letter = numberToLetter[new_number]
+  return new_letter.upcase if is_upper
+
+  new_letter
 end
 
 ##
@@ -19,16 +14,21 @@ end
 # @param [String] string_to_encode
 # @Param [Number] shift_amount
 # @return [String] the encoded string
-def self.caesar_cipher(string_to_encode, shift_amount, alphabet = "abcdefghijklmnopqrstuvwxyz")
-    encoded_string = ""
+def self.caesar_cipher(string_to_encode, shift_amount, alphabet = 'abcdefghijklmnopqrstuvwxyz')
+  encoded_string = ''
 
-    # Create the alphabet
-    letterToNumber = Hash.new()
-    numberToLetter = Hash.new()
-    alphabet.split("").each_with_index {|val, index| letterToNumber[val] = index; numberToLetter[index] = val}
+  # Create the alphabet
+  letterToNumber = {}
+  numberToLetter = {}
+  alphabet.split('').each_with_index do |val, index|
+    letterToNumber[val] = index
+    numberToLetter[index] = val
+  end
 
-    string_to_encode.split("").each {|val| encoded_string.concat(encode_letter(val, shift_amount, letterToNumber, numberToLetter, alphabet.length))}
+  string_to_encode.split('').each do |val|
+    encoded_string.concat(encode_letter(val, shift_amount, letterToNumber, numberToLetter, alphabet.length))
+  end
 
-    return encoded_string
+  encoded_string
 end
-puts(caesar_cipher("What a string!", 5))
+puts(caesar_cipher('What a string!', 5))
