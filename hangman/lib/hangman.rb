2b04b50c5235c2dd09d82785b6fac12563941e16
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2024-07-27 19:26:43 +0100

    Finished hangman

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -1,3 +1,31 @@
+require 'json'
+
+class HangmanSave
+  attr_accessor :word_to_guess, :display_word, :incorrect_letters, :correct_letters, :number_guesses
+
+  def initialize(word_to_guess, display_word, incorrect_letters, correct_letters, number_guesses)
+    @word_to_guess = word_to_guess
+    @display_word = display_word
+    @incorrect_letters = incorrect_letters
+    @correct_letters = correct_letters
+    @number_guesses = number_guesses
+  end
+
+  def to_json
+    JSON.dump({
+      :word_to_guess => word_to_guess,
+      :display_word => display_word,
+      :incorrect_letters => incorrect_letters,
+      :correct_letters => correct_letters,
+      :number_guesses => number_guesses
+    })
+  end
+
+  def self.from_json(string)
+    data = JSON.load string
+    self.new(data['word_to_guess'], data['display_word'], data['incorrect_letters'], data['correct_letters'], data['number_guesses'])
+  end
+end
 
 def get_random_word
   ret = ''
@@ -11,11 +39,11 @@ end
 def get_guess(incorrect_letters, correct_letters)
   valid = false
   until valid
-    puts 'Guess a letter'
+    puts 'Guess a letter or type save to save the game'
     input = gets
     input = input.strip
     input = input.downcase
-    valid = /([a-z]){1}/.match(input) && input.length == 1 && !correct_letters.include?(input) && !incorrect_letters.include?(input)
+    valid = input == 'save' || (/([a-z]){1}/.match(input) && input.length == 1 && !correct_letters.include?(input) && !incorrect_letters.include?(input))
   end
   input
 end
@@ -31,33 +59,58 @@ def update_display_word(word_to_guess, display_word, guess)
 end
 
 def game
-  number_guesses = 15
-
-  word_to_guess = get_random_word()
-  display_word = ''
-  display_word = display_word.rjust(word_to_guess.length, '_')
-  incorrect_letters = ['a']
-  correct_letters = []
-  stop = false
-
   puts 'Hangman'
-  puts word_to_guess
+  puts '1 - play the game'
+  puts '2 - load a game'
+  input = gets
+  input = input.strip
+  if input == '2'
+    game_data = HangmanSave.from_json(File.read('save.json').strip)
+  else
+    word_to_guess = get_random_word()
+    game_data = HangmanSave.new(
+      word_to_guess,
+      ''.rjust(word_to_guess.length, '_'),
+      [],
+      [],
+      15
+    )
+  end
+  stop = false
+  saving = false
+  
+  # puts game_data.word_to_guess
 
   until stop
-    
-    puts "#{display_word}\nNumber of guesses remaining #{number_guesses}. Incorrect letters: #{incorrect_letters.join(', ')}."
-    guess = get_guess(incorrect_letters, correct_letters)
-    if word_to_guess.include?(guess)
-      correct_letters.push(guess)
-      display_word = update_display_word(word_to_guess, display_word, guess)
-      stop = !display_word.include?('_')
-    else
-      incorrect_letters.push(guess)
-    end
+    puts "#{game_data.display_word}\nNumber of guesses remaining #{game_data.number_guesses}. Incorrect letters: #{game_data.incorrect_letters.join(', ')}."
+    guess = get_guess(game_data.incorrect_letters, game_data.correct_letters)
 
-    number_guesses -= 1
-    if number_guesses <= 0
+    if guess == 'save'
       stop = true
+      saving = true
+    else
+      if game_data.word_to_guess.include?(guess)
+        game_data.correct_letters.push(guess)
+        game_data.display_word = update_display_word(game_data.word_to_guess, game_data.display_word, guess)
+        stop = !game_data.display_word.include?('_')
+      else
+        game_data.incorrect_letters.push(guess)
+      end
+
+      game_data.number_guesses -= 1
+      if game_data.number_guesses <= 0
+        stop = true
+      end
+    end
+  end
+
+  if !saving && game_data.display_word.include?('_')
+    puts "You lost. The word was #{game_data.word_to_guess}."
+  elsif !saving
+    puts "You won! The word was #{game_data.word_to_guess}."
+  elsif saving
+    File.open('save.json', 'w') do |file|
+      file.puts game_data.to_json
     end
   end
 end
