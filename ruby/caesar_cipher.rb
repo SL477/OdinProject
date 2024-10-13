a2f0c1beba51e911975257f495492377a7e66a08
Tom Fishwick <fishwickt@gmail.com>
2024-10-13 17:37:19 +0100

    Caesar Cipher

--- ruby/caesar_cipher.rb
+++ ruby/caesar_cipher.rb
@@ -1,9 +1,9 @@
-def self.encode_letter(letter_to_encode, shift_amount, letterToNumber, numberToLetter, alphabet_length)
+def encode_letter(letter_to_encode, shift_amount, letter_to_number, number_to_letter, alphabet_length) # rubocop:disable Style/FrozenStringLiteralComment
   is_upper = letter_to_encode.upcase == letter_to_encode
-  return letter_to_encode unless letterToNumber.key?(letter_to_encode.downcase)
+  return letter_to_encode unless letter_to_number.key?(letter_to_encode.downcase)
 
-  new_number = (letterToNumber[letter_to_encode.downcase] + shift_amount) % alphabet_length
-  new_letter = numberToLetter[new_number]
+  new_number = (letter_to_number[letter_to_encode.downcase] + shift_amount) % alphabet_length
+  new_letter = number_to_letter[new_number]
   return new_letter.upcase if is_upper
 
   new_letter
@@ -14,21 +14,21 @@ end
 # @param [String] string_to_encode
 # @Param [Number] shift_amount
 # @return [String] the encoded string
-def self.caesar_cipher(string_to_encode, shift_amount, alphabet = 'abcdefghijklmnopqrstuvwxyz')
+def caesar_cipher(string_to_encode, shift_amount, alphabet = 'abcdefghijklmnopqrstuvwxyz') # rubocop:disable Metrics/MethodLength
   encoded_string = ''
 
   # Create the alphabet
-  letterToNumber = {}
-  numberToLetter = {}
-  alphabet.split('').each_with_index do |val, index|
-    letterToNumber[val] = index
-    numberToLetter[index] = val
+  letter_to_number = {}
+  number_to_letter = {}
+  alphabet.chars.each_with_index do |val, index|
+    letter_to_number[val] = index
+    number_to_letter[index] = val
   end
 
-  string_to_encode.split('').each do |val|
-    encoded_string.concat(encode_letter(val, shift_amount, letterToNumber, numberToLetter, alphabet.length))
+  string_to_encode.chars.each do |val|
+    encoded_string.concat(encode_letter(val, shift_amount, letter_to_number, number_to_letter, alphabet.length))
   end
 
   encoded_string
 end
-puts(caesar_cipher('What a string!', 5))
+# puts(caesar_cipher('What a string!', 5))
