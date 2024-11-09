1cccafd16e5022688e6e63521343672f0405126b
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2024-11-09 14:44:33 +0000

    Queen moves

--- ruby/lib/queen.rb
+++ ruby/lib/queen.rb
@@ -10,4 +10,18 @@ class Queen < ChessPiece
           end
     super(location, 9, alignment, 'Q', pic, 'queen')
   end
+
+   # Returns strings of Column-Row
+   def potential_moves(board)
+    ret = []
+    ret.push(*check_moves_in_loop(1, 1, board))
+    ret.push(*check_moves_in_loop(-1, 1, board))
+    ret.push(*check_moves_in_loop(1, -1, board))
+    ret.push(*check_moves_in_loop(-1, -1, board))
+    ret.push(*check_moves_in_loop(1, 0, board))
+    ret.push(*check_moves_in_loop(-1, 0, board))
+    ret.push(*check_moves_in_loop(0, 1, board))
+    ret.push(*check_moves_in_loop(0, -1, board))
+    ret
+  end
 end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -53,4 +53,14 @@ describe Chess do # rubocop:disable Metrics/BlockLength
     # pp moves
     expect(result).to be true
   end
+
+  it "Queen on Black C5" do
+    game = Chess.new({"c5" => '{"moved":true,"location":[4,2],"points":1,"alignment":"white","notation":"b","picture":"♙","type":"queen"}'})
+    row_col = game.get_row_column('C5')
+    moves = game.board[row_col[0]][row_col[1]].potential_moves(game.board)
+    expect(moves.length).to eq(25)
+    result = moves.include?(:'06')
+    # pp moves
+    expect(result).to be true
+  end
 end
