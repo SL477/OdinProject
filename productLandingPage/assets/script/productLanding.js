9523f16f5159fa21038ba3e6a573dac1f932a288
Tom Fishwick <fishwickt@gmail.com>
2025-03-02 17:54:26 +0000

    Fix removing bootstrap issues
    Add dark mode

--- productLandingPage/assets/script/productLanding.js
+++ productLandingPage/assets/script/productLanding.js
@@ -8,3 +8,12 @@ function setCurrentYear() {
     currentYear.textContent = d.getFullYear();
 }
 setCurrentYear();
+
+const dialog = document.querySelector('dialog');
+const closeButton = document.querySelector('dialog button');
+const openButton = document.getElementById('infoModalButton');
+const infoModalFirstBtn = document.getElementById('infoModalFirst');
+
+openButton.addEventListener('click', () => dialog.showModal());
+closeButton.addEventListener('click', () => dialog.close());
+infoModalFirstBtn.addEventListener('click', () => dialog.showModal());
