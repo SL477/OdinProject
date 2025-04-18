d195f1b0462a6951c39803a53a54388b2214691d
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2025-04-18 17:32:08 +0100

    Entry form

--- library/assets/js/library.mjs
+++ library/assets/js/library.mjs
@@ -2,6 +2,10 @@ import { Book } from './book.mjs';
 
 const myLibrary = [];
 const libraryUL = document.getElementById('library');
+const openNewBookBtn = document.getElementById('openNewBook');
+const newBookDia = document.getElementById('newBook');
+const newBookCloseBtn = document.getElementById('newBookClose');
+const newBookForm = document.getElementById('newBookForm');
 
 /** take params, create a book then store it in the array
  * @param {string} title
@@ -13,14 +17,39 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
 
-function main() {
-    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
-    if (myLibrary) {
+function displayLibrary() {
+    if (libraryUL) {
+        libraryUL.innerHTML = '';
         for (const item of myLibrary) {
             const libraryLI = document.createElement('li');
             libraryLI.textContent = item.info();
             libraryUL.appendChild(libraryLI);
         }
+    }
+}
+
+function main() {
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+    displayLibrary();
+
+    if (openNewBookBtn && newBookDia && newBookCloseBtn) {
+        openNewBookBtn.addEventListener('click', () => newBookDia.showModal());
+        newBookCloseBtn.addEventListener('click', () => newBookDia.close());
+    }
+
+    if (newBookForm) {
+        newBookForm.addEventListener('submit', (ev) => {
+            const formData = Object.fromEntries(new FormData(ev.target));
+            // console.log('formData', formData);
+            addBookToLibrary(
+                formData.title,
+                formData.author,
+                formData.pages,
+                formData.read
+            );
+            displayLibrary();
+            ev.target.reset();
+        });
     }
 }
 
