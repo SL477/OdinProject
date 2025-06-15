ed9ebff7e654728f54a0bc439aa3b6bd25790843
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2025-06-15 14:05:11 +0100

    Add button to add project

--- todos/assets/js/main.mjs
+++ todos/assets/js/main.mjs
@@ -1,16 +1,26 @@
 import TODO from './todo.mjs';
+import Project from './project.mjs';
 
 const todoAddBtn = document.getElementById('todoAddBtn');
 const todoForm = document.getElementById('todoForm');
 const todoCloseBtn = document.getElementById('todoCloseBtn');
 const todoFormFm = document.getElementById('todoFormFm');
+const projectAddBtn = document.getElementById('projectAddBtn');
+const projectForm = document.getElementById('projectForm');
+const projectCloseBtn = document.getElementById('projectCloseBtn');
+const projectFormFm = document.getElementById('projectFormFm');
 
 if (todoAddBtn && todoForm && todoCloseBtn) {
     todoAddBtn.addEventListener('click', () => todoForm.showModal());
     todoCloseBtn.addEventListener('click', () => todoForm.close());
 }
 
-//#region TODOForm
+if (projectAddBtn && projectForm && projectCloseBtn) {
+    projectAddBtn.addEventListener('click', () => projectForm.showModal());
+    projectCloseBtn.addEventListener('click', () => projectForm.close());
+}
+
+//#region TODO Form
 if (todoFormFm) {
     todoFormFm.addEventListener('submit', () => {
         const todoData = new FormData(todoFormFm);
@@ -23,6 +33,20 @@ if (todoFormFm) {
             todoData.get('isDone')
         );
         console.log(newTodo);
+    });
+}
+//#endregion
+
+//#region Project Form
+if (projectFormFm) {
+    projectFormFm.addEventListener('submit', () => {
+        const projectData = new FormData(projectFormFm);
+        const newProject = new Project(
+            projectData.get('code'),
+            projectData.get('notes'),
+            []
+        );
+        console.log(newProject);
     });
 }
 //#endregion
