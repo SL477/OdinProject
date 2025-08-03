a7c0c67c4a6f32c9a94072543163d3ecf022bcf3
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2025-08-03 12:24:42 +0100

    Caesar Cipher

---
+++ testing/caesarCipher.test.js
@@ -0,0 +1,13 @@
+const caesarCipher = require('./caesarCipher.js');
+
+test('Wrapping', () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc');
+});
+
+test('case preservation', () => {
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+});
+
+test('punctuation', () => {
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+});
